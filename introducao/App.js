9e41ec4431654a7f0462d10bd0a455e0a6e56b0f
kaiquejorge <kaiquejor@gmail.com>
2023-08-20 22:44:25 -0300

    update

--- introducao/App.js
+++ introducao/App.js
@@ -1,12 +1,13 @@
 import { StyleSheet, View } from 'react-native';
 //import HelloRN from './components/01HelloRN';
-import HelloRN2 from './components/02HelloRN2';
+//import HelloRN2 from './components/02HelloRN2';
+import Cat from './components/03FullName';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HelloRN2 />
+      <Cat />
     </View>
   );
 }
---
+++ introducao/components/03FullName.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const getFullName = (
+  firstName: string,
+  secondName: string,
+  thirdName: string,
+) => {
+  return firstName + ' ' + secondName + ' ' + thirdName;
+};
+
+const Cat = () => {
+  return 
+    <Text>
+        Hello, I am {getFullName('Rum', 'Tum', 'Tugger')}!
+    </Text>;
+};
+
+export default Cat;
